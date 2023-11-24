027fb307ca0e23dc83ec1da6ad5ecd7594b2c21f
minzziPark <minzzi0068@gmail.com>
2023-11-24 23:45:36 +0900

    페이지별 route 구성

--- uni_project/src/App.js
+++ uni_project/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import MainPage from "./MainPage";
+import Select from "./Select";
+import Result from "./Result";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/select" element={<Select />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
