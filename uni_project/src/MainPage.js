9faab0445a878d8ec65f7962b1c0022beea4d9b9
LYH <peperony367@gmail.com>
2023-11-25 00:04:15 +0900

    메인페이지 버튼

--- uni_project/src/MainPage.js
+++ uni_project/src/MainPage.js
@@ -1,3 +1,14 @@
-export default function MainPage() {
-  return <></>;
+import React from "react";
+import {Link} from "react-router-dom";
+
+function MainPage() {
+  return (
+    <div>
+      <button>
+        시작하기
+      </button>
+    </div>
+  );
 }
+
+export default MainPage;
